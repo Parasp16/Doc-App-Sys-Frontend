0d74f084159cf0984134b91cbfab20d093d84580
Paras Pawaskar <pawaskarparas@gmail.com>
2026-01-17 20:38:16 +0530

    Remove unnecessary comments in ProtectedRoute

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -3,13 +3,12 @@ import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = () => {
   const token = localStorage.getItem("token6163");
 
-  // ❌ Not logged in
   if (!token) {
     return <Navigate to="/" replace />;
   }
 
-  // ✅ Logged in
   return <Outlet />;
 };
+
 
 export default ProtectedRoute;
